a78c6509246ec403ba864e9da8e496c0f2213fe1
Jaydeep-Ramanuj-tops <jaydeepramanuj.tops@gmail.com>
2024-07-06 18:50:11 +0530

    Module 2 Lecture 1 & 2 6-7

---
+++ Module 2_C/variable-6-7.c
@@ -0,0 +1,72 @@
+// Preprocessor
+
+#include <stdio.h>
+
+
+// starting of my file
+void main(){
+
+
+// Variable declaration
+
+
+// syntax
+
+// data_type variable_name;
+int num;
+
+
+// Variable assignment
+num=10;
+printf("\nValue before update: %d",num);
+
+
+// Variable value updation
+num=30;
+printf("\nValue after update: %d",num);
+
+
+
+// Variable declaration + Assignment
+int number=num;
+
+
+// Multiple variable declaration
+int num1,num2;
+
+
+printf("\nMy number is: %d",num);
+
+
+// Naming conventions
+
+//int 123=5132; // Error
+int a123=354; // Valid
+
+//int a@1=123; // Error
+int a_1=123; // Valid
+int __=3; // Valid
+
+// int my number=123; // Error
+int myNumber=123;
+
+// Casing
+int prince_is_a_smart_guy; // Valid, snake_Case, preferrablle in python
+int princeIsASmartGuy; // Valid, camelCase
+int PrinceIsASmartGuy; // Valid, PascalCase
+// int Prince-is-a-smart-guy; // Valid, PascalCase
+
+
+// Keywords, These are words which are reserved by language,
+// which preset meaning
+
+// int int =4; // Error
+
+// 1. You can't start variable name with number
+// 2. You cannot use special character inside variable name,
+// except underscore (_)
+// 3. You cannot use space in between variable name
+// 4. You cannot use keywords as variable name
+
+}
+
