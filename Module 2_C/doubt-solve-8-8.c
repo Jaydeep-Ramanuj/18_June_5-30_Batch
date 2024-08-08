74e704cf00cfa17904e34d47b46ad3184ae9ec2d
Jaydeep-Ramanuj-tops <jaydeepramanuj.tops@gmail.com>
2024-08-08 18:51:13 +0530

    Module 2 Lecture 14 8-8

---
+++ Module 2_C/doubt-solve-8-8.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int number[5] = {10, 20, 30, 40, 50};
+
+    printf("%d", *number);
+    printf("\n %d", *number);
+    // printf("\n %p", &number[0]);
+
+    return 0;
+}
--- Module 2_C/file-system-6-8.c
+++ Module 2_C/file-system-6-8.c
@@ -2,10 +2,34 @@
 
 void main()
 {
+
+    // syntax
+    // FILE *file_object;
+
     FILE *file;
     char data[100];
-    file = fopen("data.txt", "r");
+
+    // fopen("file_path_along_with_extension","mode")
+
+    // file = fopen("data.txt", "r");
+    // file = fopen("data.txt", "w");
+    file = fopen("data.txt", "a");
+
     // data[100] = fscanf(file, "%s", 50);
-    fgets(data, 500, file);
-    printf("%s", data);
+    // fgets(data, 500, file);
+    // printf("%s", data);
+
+    // r => readonly, it allows only read functions
+    // w => write, we can write data in file, but it will replace already existing data in file
+    // a => append, we can append data in file, it will not replace already existing data but will be added after it
+
+    // r+, read + write
+    // w+, read + write
+    // a+, read + append
+
+    // If you want to read,write or append binary values then we have modes like rb,wb,ab,rb+, etc.
+
+    fprintf(file, "\nThis text will be appended");
+
+    fclose(file);
 }
