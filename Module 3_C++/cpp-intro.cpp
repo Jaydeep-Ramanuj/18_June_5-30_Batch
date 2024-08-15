426673ec6894f8ce93b4bb69f7b9273bab8b1db3
Jaydeep-Ramanuj-tops <jaydeepramanuj.tops@gmail.com>
2024-08-15 18:40:21 +0530

    Module 3 Lecture 1 15-8

---
+++ Module 3_C++/cpp-intro.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // std::cout << "Namste C++";
+    cout << "Namste C++" << endl;
+
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Name: " << name;
+
+    return 0;
+}
